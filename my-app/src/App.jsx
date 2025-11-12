092154f00b44f3979de3f855c2725982908499e5
fachrul fatanillah <87415039+fachrulfatanillah@users.noreply.github.com>
2025-11-13 01:23:31 +0700

    props pada component Class & Function

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -8,10 +8,10 @@ function App() {
     <div className="App">
         <h1>Cafe and Resto Codepolitan</h1>
         <h2>Menu Makanan</h2>
-        <MenuMakanan/>
+        <MenuMakanan namaMenu={"Ayam Bakar"} hargaMenu={"25000"}/>
         
         <h2>Menu Minuman</h2>
-        <MenuMinuman />
+        <MenuMinuman namaMenu={"Jus Jeruk"} hargaMenu={"10000"}/>
       </div>
   )
 }
--- my-app/src/components/MenuMinuman/MenuMinuman.jsx
+++ my-app/src/components/MenuMinuman/MenuMinuman.jsx
@@ -4,8 +4,8 @@ class MenuMinuman extends Component {
     render() {
         return (
             <div style={{border: "1px solid black", width: 300, margin: 'auto'}}>
-                <p>Nama Menu: Juice Melon</p>
-                <p>Harga: Rp. 15.000</p>
+                <p>Nama Menu: {this.props.namaMenu}</p>
+                <p>Harga: Rp. {this.props.hargaMenu}</p>
           </div>
         )
     }
