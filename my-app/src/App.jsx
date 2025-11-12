b76b5072097215d1cc563e8352520d9cc2227c20
fachrul fatanillah <87415039+fachrulfatanillah@users.noreply.github.com>
2025-11-13 03:55:00 +0700

    Conditional Rendering

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -5,6 +5,16 @@ import MenuMinuman from './components/MenuMinuman/MenuMinuman'
 
 function App() {
   var [namaResto, setNamaResto] = useState("Nama Resto");
+  var [daftarMenu, setDaftarMenu] = useState(
+    [{ 'namaMenu' : 'Ayam Bakar',
+      'hargaMenu' : 25000,
+      'stokMenu' : 10
+    },
+    { 'namaMenu' : 'Ikan Bakar',
+      'hargaMenu' : 28000,
+      'stokMenu' : 0
+    },]
+  );
   
   useEffect(() => {
     setNamaResto("Cafe and Resto Codepolitan");
@@ -19,7 +29,9 @@ function App() {
         <h1>{namaResto}</h1>
         <button onClick={()=> handleBtnOnChange('Programming')}>Ganti Nama</button>
         <h2>Menu Makanan</h2>
-        <MenuMakanan namaMenu={"Ayam Bakar"} hargaMenu={"25000"}/>
+        <MenuMakanan namaMenu={daftarMenu[0].namaMenu} hargaMenu={daftarMenu[0].hargaMenu} stokMenu={daftarMenu[0].stokMenu}/>
+        <br />
+        <MenuMakanan namaMenu={daftarMenu[1].namaMenu} hargaMenu={daftarMenu[1].hargaMenu} stokMenu={daftarMenu[1].stokMenu}/>
         
         <h2>Menu Minuman</h2>
         <MenuMinuman namaMenu={"Jus Jeruk"} hargaMenu={"10000"}/>
--- my-app/src/components/MenuMakanan/MenuMakanan.jsx
+++ my-app/src/components/MenuMakanan/MenuMakanan.jsx
@@ -1,9 +1,13 @@
 var MenuMakanan = (props) => {
     return (
-        <div style={{border: "1px solid black", width: 300, margin: 'auto'}}>
-            <p>Nama Menu: {props.namaMenu}</p>
-            <p>Harga: Rp. {props.hargaMenu}</p>
-          </div>
+        props.stokMenu>0 ? 
+            <div style={{border: "1px solid black", width: 300, margin: 'auto'}}>
+                <p>Nama Menu: {props.namaMenu}</p>
+                <p>Harga: Rp. {props.hargaMenu}</p>
+                <p>Stok: Rp. {props.stokMenu}</p>
+            </div>
+        :
+            null
     )
 }
 
