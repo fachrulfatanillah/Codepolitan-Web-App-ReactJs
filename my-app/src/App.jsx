52b98915fe034c9bdd6439a5cd5709f684fffb4e
fachrul fatanillah <87415039+fachrulfatanillah@users.noreply.github.com>
2025-11-13 20:23:23 +0700

    React Form onChange()

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -34,6 +34,10 @@ function App() {
       },
     ]
   });
+
+  var [nama, setNama] = useState("");
+  var [keterangan, setKeterangan] = useState("");
+  var [jenisPembayaran, setJenisPembayaran] = useState("");
   
   useEffect(() => {
     setNamaResto("Cafe and Resto Codepolitan");
@@ -42,11 +46,42 @@ function App() {
   var handleBtnOnChange = (props) => {
     setNamaResto(props);
   };
+
+  var handleOnChangeNama = (event) => {
+    setNama(event.target.value);
+  };
+
+  var handleOnChangeKeterangan = (event) => {
+    setKeterangan(event.target.value);
+  };
+
+  var handleOnChangeJenisPembayaran = (event) => {
+    setJenisPembayaran(event.target.value);
+  };
   
   return (
       <div className="App">
         <h1>{namaResto}</h1>
         <button onClick={()=> handleBtnOnChange('Programming')}>Ganti Nama</button>
+
+        <form >
+          <label style={{ display: 'block', fontWeight: 'bold' }}>Nama:</label>
+          <input type="text" style={{ minWidth: 300 }} name='inputNama' onChange={handleOnChangeNama}/>
+          <small style={{ display: 'block'}}>Nama: {nama}</small>
+
+          <label style={{ display: 'block', fontWeight: 'bold' }}>Keterangan:</label>
+          <textarea style={{ minWidth: 300, minHeight: 100 }} name="inputKeterangan" onChange={handleOnChangeKeterangan}></textarea>
+          <small style={{ display: 'block' }}>Keterangan: {keterangan}</small>
+
+          <label style={{ display: 'block', fontWeight: 'bold' }}>Jenis Pembayaran: </label>
+          <select name="jenisPembayaran" style={{ minWidth: 300 }} onChange={handleOnChangeJenisPembayaran}>
+            <option value="Cash">Cash</option>
+            <option value="Debit">Debit</option>
+            <option value="Voucher">Voucher</option>
+          </select>
+          <small style={{ display: 'block' }}>Jenis Pembayaran: {jenisPembayaran} </small>
+        </form>
+
         <h2>Menu Makanan</h2>
         <MenuMakanan namaMenu={daftarMenu.makanan[0].namaMenu} hargaMenu={daftarMenu.makanan[0].hargaMenu} stokMenu={daftarMenu.makanan[0].stokMenu}/>
         <br />
