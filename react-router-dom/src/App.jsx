03a2d19c3050964d2d5a11a36441240057ba7cdb
fachrul fatanillah <87415039+fachrulfatanillah@users.noreply.github.com>
2025-11-15 16:33:47 +0700

    Routing Sederhana

--- react-router-dom/src/App.jsx
+++ react-router-dom/src/App.jsx
@@ -1,11 +1,26 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
 function App() {
-
   return (
-    <>
-      
-    </>
+    <Router>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <Link className="navbar-brand" to="/">Codepolitan</Link>
+          <div className="navbar-nav">
+            <Link className="nav-item nav-link" to="/">Home</Link>
+            <Link className="nav-item nav-link" to="/premium">Premium</Link>
+            <Link className="nav-item nav-link" to="/devschool">DevSchool</Link>
+          </div>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<h4>Home</h4>} />
+          <Route path="/premium" element={<h4>Premium</h4>} />
+          <Route path="/devschool" element={<h4>DevSchool</h4>} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
